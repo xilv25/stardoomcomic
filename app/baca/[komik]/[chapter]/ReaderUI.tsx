1c4ee6d7be3c688dec62a2d6f6dae3253a6d66a3
xilv25 <thoriqrizqifahmi@gmail.com>
2026-09-14 21:00:51 +0700

    Update ReaderUI.tsx

--- app/baca/[komik]/[chapter]/ReaderUI.tsx
+++ app/baca/[komik]/[chapter]/ReaderUI.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
-import { supabase } from '../utils/supabase'; // Sesuaikan path utils supabase jika berbeda
+import { supabase } from '../../../../utils/supabase';// Sesuaikan path utils supabase jika berbeda
 
 export default function ReaderUI({ 
   komik, chapter, chapterData, mangaData, prevCh, nextCh, judulKomik, namaChapter 
