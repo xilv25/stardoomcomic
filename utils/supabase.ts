253209f0f10e63a17b6ce098c178e4341e672339
xilv25 <thoriqrizqifahmi@gmail.com>
2026-09-14 19:41:27 +0700

    Update supabase.ts

--- utils/supabase.ts
+++ utils/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || '';
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || '';
+// Pakai "as string" agar proses build Vercel (TypeScript) tidak protes
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
